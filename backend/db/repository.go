ec23c2790694323845b3e34a241bb7ebc99602f1
rshimaok <rihs14804@gmail.com>
2023-05-22 14:48:27 +0900

    initial commit

--- backend/db/repository.go
+++ backend/db/repository.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"database/sql"
 
-	"github.com/mercari-build/mecari-build-hackathon-2023/backend/domain"
+	"github.com/riho8/mecari-build-hackathon-2023/backend/domain"
 )
 
 type UserRepository interface {
